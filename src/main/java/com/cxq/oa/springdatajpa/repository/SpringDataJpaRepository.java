b00dfc2a62f1c87724555cd3f6e65678deea8a9f
caixiongqi <caixiongqi@sinosoft.com.cn>
2018-05-09 15:57:37 +0800

    加入修改方法例子

--- src/main/java/com/cxq/oa/springdatajpa/repository/SpringDataJpaRepository.java
+++ src/main/java/com/cxq/oa/springdatajpa/repository/SpringDataJpaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -26,5 +27,15 @@ public interface SpringDataJpaRepository extends JpaRepository<User, String>, Jp
      */
     @Query(value = " select * from user where user_id = ?1", nativeQuery = true)
     User getUserByUserId(String userId);
+
+    /**
+     * 修改方法，加入modifying注解
+     * @param userName 姓名
+     * @param userId id
+     * @return 修改后的实体
+     */
+    @Modifying
+    @Query(value = " update user set user_name = ?1 where user_id = ?2")
+    User saveUser(String userName, String userId);
 
 }
